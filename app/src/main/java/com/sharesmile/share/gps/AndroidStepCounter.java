350bc694dde47e483cfbec99c3d5c31235dc943a
Ankit Maheshwari <ankit@jumbotail.com>
2016-05-12 01:36:23 +0530

    Added AndroidStepCounter

---
+++ app/src/main/java/com/sharesmile/share/gps/AndroidStepCounter.java
@@ -0,0 +1,26 @@
+package com.sharesmile.share.gps;
+
+import android.content.Intent;
+
+/**
+ * Created by ankitm on 12/05/16.
+ */
+public class AndroidStepCounter implements StepCounter {
+
+    private static final String TAG = "AndroidStepCounter";
+
+    @Override
+    public void startCounting() {
+
+    }
+
+    @Override
+    public void stopCounting() {
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    }
+}
