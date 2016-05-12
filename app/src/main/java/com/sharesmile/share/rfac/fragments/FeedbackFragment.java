528104141fbdae1a9a0a75dbabd40a8ebc7085f9
Anshul Agarwal <anshul1.agarwal@peppertap.com>
2016-05-12 15:28:46 +0530

    update toolbar title

--- app/src/main/java/com/sharesmile/share/rfac/fragments/FeedbackFragment.java
+++ app/src/main/java/com/sharesmile/share/rfac/fragments/FeedbackFragment.java
@@ -47,6 +47,6 @@ public class FeedbackFragment extends BaseFragment implements View.OnClickListen
 
         //submit feedback to server
 
-        getFragmentController().addFragment(new OnScreenFragment(),true);
+        getFragmentController().replaceFragment(new OnScreenFragment(),true);
     }
 }
