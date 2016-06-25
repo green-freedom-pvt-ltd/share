daf09daff7f26e48a10e58c8b70b1bb902786958
Anshul Agarwal <anshul1.agarwal@peppertap.com>
2016-06-25 21:40:56 +0530

    onboarding screen

--- app/src/main/java/com/sharesmile/share/core/Constants.java
+++ app/src/main/java/com/sharesmile/share/core/Constants.java
@@ -89,6 +89,7 @@ public class Constants {
 
     public static final String PREF_IS_LOGIN  = "pref_user_login";
     public static final String PREF_USER_ID = "pref_user_id";
+    public static final String PREF_FIRST_TIME_USER = "pref_first_time_user";
 
 
     //User Data
