3230baf85ba9db0d78989cfb8b707d5cd5f5eeda
ankitm <maheshwari.ankit.iitd@gmail.com>
2017-01-24 12:42:53 +0530

    Added some comments

--- app/src/main/java/com/sharesmile/share/core/IFragmentController.java
+++ app/src/main/java/com/sharesmile/share/core/IFragmentController.java
@@ -1,11 +1,17 @@
 package com.sharesmile.share.core;
 
 /**
+ * Controller for fragments, it contains methods which invoked by fragments and implemented by Activity holding those fragments
  * Created by ankitmaheshwari1 on 11/01/16.
  */
 public interface IFragmentController {
 
     String TAG = "IFragmentController";
+
+    /*
+        Operation codes
+        Each operation code denotes a unique operation which is requested by fragment and performed by activity
+     */
 
     int END_RUN_START_COUNTDOWN = 100;
 
@@ -19,6 +25,10 @@ public interface IFragmentController {
 
     int SHOW_MESSAGE_CENTER = 105;
     int SHOW_LEAGUE_ACTIVITY = 106;
+
+    /*
+        Common methods which can be implemented either by abstract BaseActivity or by solid child activities
+     */
 
     void addFragment(BaseFragment fragment, boolean addToBackStack);
 
