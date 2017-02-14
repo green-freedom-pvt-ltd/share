8439fff7f6a7c17af9267075cc37094599dbd6dc
ankitm <maheshwari.ankit.iitd@gmail.com>
2017-02-14 13:37:22 +0530

    Fixed crash on start of share screen

--- app/src/main/java/com/sharesmile/share/rfac/fragments/ShareFragment.java
+++ app/src/main/java/com/sharesmile/share/rfac/fragments/ShareFragment.java
@@ -10,14 +10,12 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.graphics.drawable.VectorDrawableCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -141,9 +139,6 @@ public class ShareFragment extends BaseFragment implements View.OnClickListener,
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        RadioButton button = (RadioButton)mShareGroup.findViewById(R.id.fb);
-        button.setButtonDrawable(VectorDrawableCompat.create(getResources(), R.drawable.selector_fb_radio, null));
 
         View view = inflater.inflate(R.layout.fragment_share, null);
         ButterKnife.bind(this, view);
