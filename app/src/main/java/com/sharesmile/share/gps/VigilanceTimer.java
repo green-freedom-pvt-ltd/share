86235950a2c51640fd74b18d2a8b3a5c6d488176
ankitm <maheshwari.ankit.iitd@gmail.com>
2017-06-24 15:48:55 +0530

    Added comments explaining USAIN_BOLT logic in VigilanceTimer

--- app/src/main/java/com/sharesmile/share/gps/VigilanceTimer.java
+++ app/src/main/java/com/sharesmile/share/gps/VigilanceTimer.java
@@ -118,16 +118,24 @@ public class VigilanceTimer implements Runnable {
 		return false;
 	}
 
+	/**
+	 * This check is triggered after every VIGILANCE_TIMER_INTERVAL (17 secs)
+	 * @return true if we believe that user is in a vehicle
+	 */
 	private boolean checkForTooFast(){
 		if (!Config.USAIN_BOLT_CHECK){
 			return false;
 		}
 
+		// recentSpeed is avg speed from recent few samples of accepted GPS points in the past 24 secs
 		float recentSpeed = workoutService.getCurrentSpeed();
-		// If recentSpeed is above lower bounds then only enter the check.
+		// If recentSpeed is above USAIN_BOLT_RECENT_SPEED_LOWER_BOUND (14.8 km/hr) then only enter the check for USAIN_BOLT
 		if (recentSpeed > Config.USAIN_BOLT_RECENT_SPEED_LOWER_BOUND){
 
+			// recent is avg GPS speed from recent few samples of accepted GPS points in the past 24 secs
+			// GPS speed is obtained directly from location object and is calculated using doppler shift
 			float recentGpsSpeed = GoogleLocationTracker.getInstance().getRecentGpsSpeed();
+			// If recentGpsSpeed is above USAIN_BOLT_GPS_SPEED_LIMIT (24 km/hr) then user must be in a vehicle
 			if (recentGpsSpeed > USAIN_BOLT_GPS_SPEED_LIMIT){
 				Logger.d(TAG, "Recent GPS speed is greater than threshold, must be Usain Bolt");
 				AnalyticsEvent.create(Event.ON_USAIN_BOLT_ALERT)
@@ -139,6 +147,7 @@ public class VigilanceTimer implements Runnable {
 				return true;
 			}
 
+			// If ActivityDetector says that user is in a vehicle with confidence then user must be in a vehicle
 			if (ActivityDetector.getInstance().isIsInVehicle()){
 				Logger.d(TAG, "ActivityRecognition detected IN_VEHICLE, must be Usain Bolt");
 				AnalyticsEvent.create(Event.ON_USAIN_BOLT_ALERT)
@@ -149,17 +158,27 @@ public class VigilanceTimer implements Runnable {
 						.buildAndDispatch();
 				return true;
 			}
+			// Go for speed logic based check with recent speed as input
 			return tooFastSecondaryCheck(recentSpeed);
 		}
 		return false;
 	}
 
+	/**
+	 * Returns true if it thinks the user is in a vehicle as per speed calculations
+	 * @param recentSpeed
+	 * @return
+	 */
 	private boolean tooFastSecondaryCheck(float recentSpeed){
-
+		/*
+		  We store recordedTime (as lastValidatedRecordedTimeInSecs), distanceCovered (as lastValidatedDistance)
+		  and numSteps (as lastValidatedNumSteps) every time this check is cleared successfully
+		  */
 		float currentRecordedTime = workoutService.getTracker().getRecordedTimeInSecs();
 		float currentDistanceCovered = workoutService.getTotalDistanceCoveredInMeters();
 		int currentNumSteps = workoutService.getTotalStepsInWorkout();
 
+		// Proceed for check only when we have recorded more distance after the last time recorded distance was validated
 		if (currentRecordedTime > lastValidatedRecordedTimeInSecs){
 			float distanceInSession = currentDistanceCovered - lastValidatedDistance;
 			int stepsInSession = currentNumSteps - lastValidatedNumSteps;
@@ -168,8 +187,10 @@ public class VigilanceTimer implements Runnable {
 					+ ", timeElapsedInSecs = " + timeElapsedInSecs
 					+ ", total distanceCovered = " +  workoutService.getTracker().getTotalDistanceCovered());
 
+			// Calculate speed in the last recorded session
 			float speedInSession = distanceInSession / timeElapsedInSecs;
 
+			// Calculate avgStrideLength (distance covered in one foot step) of the user
 			float averageStrideLength = (Utils.getAverageStrideLength() == 0)
 					? (Config.GLOBAL_AVERAGE_STRIDE_LENGTH) : Utils.getAverageStrideLength();
 			// Normalising averageStrideLength obtained
@@ -179,19 +200,37 @@ public class VigilanceTimer implements Runnable {
 			if (averageStrideLength > 1f){
 				averageStrideLength = 1f;
 			}
+			// Calculate expected num of steps in the recorded session based on speed and avgStrideLength
 			int expectedNumOfSteps = (int) (distanceInSession / averageStrideLength);
+			// ratio between actual steps counted and the expected num of steps in recorded session
 			float stepsRatio = ( (float) stepsInSession / (float) expectedNumOfSteps);
 
+			// Distance is above the threshold minimum MIN_DISTANCE_FOR_VIGILANCE (50 m) to apply Usain Bolt Filter
 			if (distanceInSession > Config.MIN_DISTANCE_FOR_VIGILANCE){
-				// Distance is above the threshold minimum to apply Usain Bolt Filter
-				float upperSpeedLimit = ActivityDetector.getInstance().isOnFoot()
-						? Config.USAIN_BOLT_UPPER_SPEED_LIMIT_ON_FOOT : Config.USAIN_BOLT_UPPER_SPEED_LIMIT;
-				float ratio = ((float) Config.VIGILANCE_TIMER_INTERVAL) / ((float) Config.CURRENT_SPEED_VALIDITY_THRESHOLD_INTERVAL);
-				float recentSpeedFactor = ratio + (1-ratio)*0.25f;
-				// Speed limit is greater for ON_FOOT cases to reduces Usain Bolt false positive occurrences
-				if ( (speedInSession > upperSpeedLimit) && (recentSpeed > upperSpeedLimit*recentSpeedFactor) ){
-					// Running faster than Usain Bolt
+
+				// If user is onFoot (less probability of user being in vehicle) then
+				// speed limit is different then when the user is not onFoot (more probability of being in vehicle).
+				// speed limit is greater for ON_FOOT cases to reduces Usain Bolt false positive occurrences
+				float upperSpeedLimit;
+				if (ActivityDetector.getInstance().isOnFoot()){
+					upperSpeedLimit = Config.USAIN_BOLT_UPPER_SPEED_LIMIT_ON_FOOT; // 45 km/hr
+				}else {
+					upperSpeedLimit = Config.USAIN_BOLT_UPPER_SPEED_LIMIT; // 25.2 km/hr
+				}
+
+				// Check if the speed is greater than allowed limit
+				if ( speedInSession > upperSpeedLimit ){
+					/*
+						 Running faster than Usain Bolt, but we have to give benefit of doubt
+						 and waive the user from USAIN_BOLT if he/she has covered
+						 a fraction (USAIN_BOLT_WAIVER_STEPS_RATIO = 0.27) of expected num of steps
+						 during this recorded session.
+						 This is done to avoid false USAIN_BOLT cases where the user is actually walking
+						 and speed goes above limit because of GPS spikes
+					 */
 					if (stepsRatio < Config.USAIN_BOLT_WAIVER_STEPS_RATIO){
+						// Speed more than limit, and insufficient num of steps,
+						// user definitely must be inside a vehicle
 						AnalyticsEvent.create(Event.ON_USAIN_BOLT_ALERT)
 								.addBundle(workoutService.getWorkoutBundle())
 								.put("detected_by", "speed_logic")
