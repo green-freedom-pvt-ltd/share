8e40d4bbc4e916ef1dd70551e3a95d7c4cef103c
parthanjaria <parthanjaria612@gmail.com>
2018-03-10 09:24:20 +0530

    added contact@impactrun.com in helpcenter level 3 screen

--- app/src/main/java/com/sharesmile/share/helpcenter/levelthree/FeedbackInputContainer.java
+++ app/src/main/java/com/sharesmile/share/helpcenter/levelthree/FeedbackInputContainer.java
@@ -1,19 +1,30 @@
 package com.sharesmile.share.helpcenter.levelthree;
 
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.text.SpannableString;
+import android.text.Spanned;
 import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.sharesmile.share.analytics.events.AnalyticsEvent;
+import com.sharesmile.share.analytics.events.Event;
 import com.sharesmile.share.core.application.MainApplication;
 import com.sharesmile.share.R;
+import com.sharesmile.share.utils.Utils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import static com.sharesmile.share.core.application.MainApplication.getContext;
 
 /**
  * Created by ankitmaheshwari on 8/29/17.
@@ -29,6 +40,9 @@ public class FeedbackInputContainer {
 
     @BindView(R.id.bt_feedback_submit)
     Button submitButton;
+
+    @BindView(R.id.help_mail_us)
+    TextView helpMailUs;
 
 
     private View containerView;
@@ -59,10 +73,30 @@ public class FeedbackInputContainer {
                 }
             });
         }
+
         promptTextView.setText(promptText);
         Typeface font = Typeface.createFromAsset(containerView.getContext().getAssets(), "fonts/Montserrat-Light.otf");
         inputEditText.setTypeface(font);
         inputEditText.setHint(hintText);
+
+        setMailStringSpannable(helpMailUs);
+    }
+
+    private void setMailStringSpannable(TextView textView) {
+        String s = getContext().getString(R.string.help_center_mail_description);
+        SpannableString ss = new SpannableString(s);
+        ss.setSpan(new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Utils.launchUri(getContext(), Uri.parse("mailto:contact@impactrun.com"));
+                AnalyticsEvent.create(Event.ON_CLICK_CONTACT_US_EMAIL_LINK)
+                        .buildAndDispatch();
+            }
+        },24,45, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setHighlightColor(
+                Color.TRANSPARENT); // prevent TextView change background when highlight
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setText(ss, TextView.BufferType.SPANNABLE);
     }
 
     public String getUserInput(){
