5e6007d01b19d177a5b741e7bf10e26eb4185a18
parthanjaria <parthanjaria612@gmail.com>
2018-03-25 18:08:01 +0530

    Change requested, added shouldpromtuser = false in stopLocationtracking()

--- app/src/main/java/com/sharesmile/share/tracking/location/GoogleLocationTracker.java
+++ app/src/main/java/com/sharesmile/share/tracking/location/GoogleLocationTracker.java
@@ -208,6 +208,7 @@ public class GoogleLocationTracker implements GoogleApiClient.ConnectionCallback
                 locationQueue.clear();
             }
             isActive = false;
+            shouldPromtUser = false;
         }
     }
 
